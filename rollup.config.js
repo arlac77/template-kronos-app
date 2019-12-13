4e9f8d240ef048f408d5b99cd775201e23fa6291
Markus Felten <Markus.Felten@gmx.de>
2019-12-13 12:24:47 +0100

    chore: initial chekin

---
+++ rollup.config.js
@@ -0,0 +1,57 @@
+import resolve from "@rollup/plugin-node-resolve";
+
+import commonjs from "rollup-plugin-commonjs";
+import executable from "rollup-plugin-executable";
+import native from "rollup-plugin-native";
+import cleanup from "rollup-plugin-cleanup";
+import consts from "rollup-plugin-consts";
+import acornClassFields from "acorn-class-fields";
+
+import builtins from "builtin-modules";
+import { name, version, description, main, module, bin } from "./package.json";
+
+const external = [...builtins, "koa", "bufferutil", "utf-8-validate"];
+
+const extensions = ["js", "mjs", "jsx", "tag"];
+const plugins = [
+  consts({
+    name,
+    version,
+    description
+  }),
+  commonjs(),
+  resolve(),
+  native(),
+  cleanup({
+    extensions
+  })
+];
+
+const config = Object.keys(bin).map(name => {
+  return {
+    input: `src/${name}-cli.mjs`,
+    output: {
+      plugins: [executable()],
+      file: bin[name]
+    }
+  };
+});
+
+if (module !== undefined && main !== undefined) {
+  config.push({
+    input: module,
+    output: {
+      file: main
+    }
+  });
+}
+
+export default config.map(c => {
+  c.output = {
+    interop: false,
+    externalLiveBindings: false,
+    format: "cjs",
+    ...c.output
+  };
+  return { acornInjectPlugins: [acornClassFields], plugins, external, ...c };
+});
