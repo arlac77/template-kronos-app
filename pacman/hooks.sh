cd3de5c6fd4693dd5611da08e986f6293899b86f
Markus Felten <Markus.Felten@db.com>
2020-04-21 00:21:44 +0200

    fix: cleanup

--- pacman/hooks.sh
+++ pacman/hooks.sh
@@ -1,8 +1,3 @@
-
-#pre_install() {
-#	useradd -U -l -M -r -s /usr/bin/nologin -d /var/lib/{{name}} -G http -c "{{description}}" {{name}}
-#}
-
 post_install() {
 	systemctl daemon-reload
 	systemctl enable {{name}}
@@ -29,6 +24,4 @@ pre_remove() {
 
 post_remove() {
 	systemctl daemon-reload
-#	userdel {{name}}
-#	groupdel {{name}}
 }
